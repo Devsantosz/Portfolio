4dd8ea1408610b985214f39f85006769bd28c409
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2026-03-10 20:18:20 -0300

    Style menu - portfolio

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,11 @@
-const botao = document.getElementById('botao-tema');
+
 const body = document.body;
 
 /* Persistência do tema */
 const temasalvo = localStorage.getItem('tema');
 temaEscuro(temasalvo === 'escuro');
 
-/* Função para alternar entre tema claro e escuro 8*/
-function temaEscuro(tipo) {
-  if (tipo == true) {
-    body.classList.add('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
-  } else {
-    body.classList.remove('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
-  }
-}
+
 
 botao.addEventListener('click', () => {
   const isescuro = body.classList.toggle('escuro');
